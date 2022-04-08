0f85d3e27e7e9ea9de8c1a53022941bcf1207d90
mzmsyk <mzmsyk.73@gmail.com>
2022-04-08 21:12:55 +0300

    AR Otizmli Bireyleri Eğitme
    Bu oyunda otizmli bireyleri eğitmek için çeşitli kategoriler hazırlanmıştır. bu kategorilerin yanı sıra ses ve animasyonlarda eklenmiştir.

---
+++ ARFİWA/Assets/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Animator[] anim;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void playAnim()
+    {
+       // anim[0].speed = 1;
+       // anim[1].speed = 1;
+       for(int i = 0; i <= 100; i++)
+        {
+            anim[i].speed = 1;
+        }
+    }
+    public void stopAnim()
+    {
+        //anim[0].speed = 0;
+        // anim[1].speed = 0;
+        for (int i = 0; i <= 100; i++)
+        {
+            anim[i].speed = 0;
+        }
+    }
+}
---
+++ ARFİWA/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,158 @@
+/*==============================================================================
+Copyright (c) 2019 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    public AudioSource ses1;
+    public AudioSource ses2;
+    public Animator animasyon1;
+    public Animator animasyon2;
+    
+   // public Canvas MyCanvas;
+   // public string animIsmi1;
+    //public string animIsmi2;
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+       // MyCanvas.gameObject.SetActive(false);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+        
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + 
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+            // animasyon1.Play(animIsmi1);
+            //  animasyon1.Play(animIsmi2);
+            ses1.Play();
+            ses2.Play();
+            animasyon1.speed = 1;
+            animasyon2.speed = 1;
+         //   MyCanvas.gameObject.SetActive(true);
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackingLost();
+            ses1.Pause();
+            ses2.Pause();
+            animasyon1.speed = 0;
+            animasyon2.speed = 0;
+           // MyCanvas.gameObject.SetActive(false);
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+            ses1.Pause();
+            ses2.Pause();
+            animasyon1.speed = 0;
+            animasyon2.speed = 0;
+          //  MyCanvas.gameObject.SetActive(false);
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = true;
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = true;
+        }
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+
+    #endregion // PROTECTED_METHODS
+}
